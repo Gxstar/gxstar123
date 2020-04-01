71571cd994c042f273132a65325f3ccb301e7cab
Gxstar <gengxing123@qq.com>
2020-04-01 21:55:32 +0800

    开始编写分类页面和路由

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='home'),
+    path('category_<int:category_id>/',views.category,name='category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from .models import Category
 # Create your views here.
 def index(request):
@@ -7,3 +7,5 @@ def index(request):
         'AllCategory':AllCategory,
     }
     return  render(request,'blog/index.html',context)
+def category(request,category_id):
+    return HttpResponse("这是分类页面%s" % category_id)
