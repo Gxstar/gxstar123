0f367945e4604b16376262f80ed32b6d14d877ca
Gxstar <gengxing123@qq.com>
2020-04-03 00:12:01 +0800

    添加登录注册功能中。。。

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path,include
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.index,name='home'),
-    path('category_<int:category_id>/',views.category,name='category')
+    path('category_<int:category_id>/',views.category,name='category'),
+    path('login/',
+         LoginView.as_view
+         (
+             template_name='blog/login.html',
+             extra_context=
+             {
+                 'title': 'Log in',
+             }
+         ),
+         name='login'),
+    path('logout/',LogoutView.as_view(next_page='blog/'), name='logout'),
 ]
