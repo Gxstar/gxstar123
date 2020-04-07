0344c641aebc9c8cbfc6831cc893c9db5719cd2e
Gxstar <gengxing123@qq.com>
2020-04-07 17:37:24 +0800

    修复登出路径bug

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path,include
+from django.urls import path,include,re_path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 from . import forms
@@ -6,6 +6,6 @@ from . import forms
 urlpatterns = [
     path('', views.index,name='home'),
     path('category_<int:category_id>/',views.category,name='category'),
-    path('logout/',LogoutView.as_view(next_page='/blog/'), name='logout'),
+    re_path(r'^(\w+)/logout/$',LogoutView.as_view(next_page='/blog/'), name='logout'),
     path('article_edit/<int:article_id>',views.article_edit,name='article_edit')
 ]
